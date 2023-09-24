169bf40fd396d4d2b66eda2184b9164145f6ae5b
Pikatoise <wierwolf@inbox.ru>
2023-09-24 19:21:33 +0500

    Я хочу умереть

--- app/src/main/java/com/example/myfinances/MainActivity.kt
+++ app/src/main/java/com/example/myfinances/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.example.myfinances
 
-import android.graphics.Color
-import android.graphics.Typeface
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
-import androidx.appcompat.app.AppCompatDelegate
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myfinances.databinding.ActivityMainBinding
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.formatter.PercentFormatter
-import com.github.mikephil.charting.utils.MPPointF
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.decodeFromString
+import kotlin.math.round
+
+@Serializable
+data class Rates(val USD: Double, val RUB: Double)
+@Serializable
+data class Data(val success: Boolean,val timestamp: Int, val base: String,val date: String,val rates: Rates)
+
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding
+	private var usdCurrency: String = "-1"
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
 		binding = ActivityMainBinding.inflate(layoutInflater)
 
 		// Change nav bar color to white
-		theme.applyStyle(R.style.Theme_GreenNavBar,false)
+		theme.applyStyle(R.style.Theme_GreenNavBar, false)
 
 		// Remove header
 		window.requestFeature(Window.FEATURE_NO_TITLE)
@@ -31,6 +46,49 @@ class MainActivity : AppCompatActivity() {
 		setContentView(binding.root)
 
 		fillPieChart(binding.pieChart)
+
+		val list = listOf("1", "4", "3", "1", "4", "3", "1", "4", "3", "1", "4", "3")
+
+		binding.lvOperationsMonth.adapter =
+			ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+		currencyApiRequest()
+	}
+
+	private fun currencyApiRequest(){
+		val url = "http://data.fixer.io/api/latest?access_key=4e07385f3f73922895d21be02e123181&symbols=USD,RUB&format=1"
+
+		val currencyFetch = OkHttpClient()
+
+		val request = Request.Builder().url(url).build()
+
+		currencyFetch.newCall(request).enqueue(object: Callback {
+			override fun onFailure(call: Call, e: IOException) {
+				Log.d("Error: Currency fetching ",e.message.toString())
+			}
+
+			override fun onResponse(call: Call, response: Response) {
+				response.use {
+					if (!response.isSuccessful){
+						Log.e("HTTP Error","Something didn't load")
+					}
+					else{
+						val body = response?.body?.string()
+
+						runOnUiThread {
+							updateCurrency(body.toString())
+						}
+					}
+				}
+			}
+		})
+	}
+
+	private fun updateCurrency(jsonData: String) {
+		var response = Json.decodeFromString<Data>(jsonData)
+
+		binding.tvCurrencyUsd.text = (round((response.rates.RUB / response.rates.USD) * 100) / 100).toString()
+		binding.tvCurrencyEuro.text = (round(response.rates.RUB * 100) / 100).toString()
 	}
 
 	private fun fillPieChart(pieChart: PieChart){
