91a3c2924286957fe3803acd5c219d1dbd9a35cc
Pikatoise <wierwolf@inbox.ru>
2024-06-14 20:54:30 +0500

    complete auth and reg activities UI

--- app/src/main/java/com/example/myfinances/ui/activities/AuthActivity.kt
+++ app/src/main/java/com/example/myfinances/ui/activities/AuthActivity.kt
@@ -1,14 +1,29 @@
 package com.example.myfinances.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myfinances.R
+import com.example.myfinances.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_auth)
+
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+
+        binding.tvAuthRegistration.setOnClickListener {
+            val i = Intent(this, RegActivity::class.java)
+
+            startActivity(i)
+
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
+
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/myfinances/ui/activities/RegActivity.kt
+++ app/src/main/java/com/example/myfinances/ui/activities/RegActivity.kt
@@ -1,14 +1,38 @@
 package com.example.myfinances.ui.activities
 
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.myfinances.R
+import com.example.myfinances.databinding.ActivityRegBinding
 
 class RegActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_reg)
+
+        binding = ActivityRegBinding.inflate(layoutInflater)
+
+        binding.mcvRegButtonRegistration.setOnClickListener {
+
+        }
+
+        setContentView(binding.root)
     }
 }
+//            val progressDialog = ProgressDialog(this)
+//            progressDialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+//            progressDialog.setContentView(R.layout.progress_dialog)
+//
+//            Handler(Looper.getMainLooper()).postDelayed({
+//                progressDialog.dismiss()
+//            }, 1000)
+//            progressDialog.show()
+//
+//            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
