efc6327891ee31b8bf4b9bef8934738d6beebe7c
Pikatoise <wierwolf@inbox.ru>
2024-06-22 22:28:11 +0500

    Change app data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 		applicationId = "com.example.myfinances"
 		minSdk = 23
 		targetSdk = 34
-		versionCode = 1
-		versionName = "1.1"
+		versionCode = 2
+		versionName = "2.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
